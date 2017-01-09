37f8cc7ea124fd17110d69c8ded780c435ee967b
okilian <okilian@ebi.ac.uk>
2017-01-09 14:53:15 +0000

    renamed ReplicaMark to ReplicaSet, because it is more a set of different replica slices than a mark like the cursorMark deleted ReplicaMark, because it seems to be copied from SolrQueryParser.java and has this history. added ReplicaSet with an empty repository history

---
+++ solr/core/src/java/org/apache/solr/search/ReplicaSet.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.solr.search;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * Created by okilian on 09/01/2017.
+ */
+public class ReplicaSet {
+
+
+  private final String repSet;
+
+  /**
+   * TODO
+   */
+  public ReplicaSet(String repSet) { this.repSet = repSet; }
+
+
+  /**
+   * TODO
+   */
+  public ReplicaSet createNext(String nextSortValues) {
+    final ReplicaSet next = new ReplicaSet(null);
+    //TODO: get the used replica set and set it
+    return new ReplicaSet("Hello");
+  }
+
+
+}
